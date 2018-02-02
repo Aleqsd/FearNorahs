a94fed80e43a9cd14116d645a97cc524858444ed
Alex <aleqsd@gmail.com>
2018-02-02 15:09:17 +0100

    End part1
    Environment and player movement

--- Assets/Scripts/WorldInteraction.cs
+++ Assets/Scripts/WorldInteraction.cs
@@ -1,16 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class WorldInteraction : MonoBehaviour {
+    NavMeshAgent playerAgent;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void Start()
+    {
+        playerAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject())
+            GetInteraction();
+    }
+    void GetInteraction()
+    {
+        Ray interactionRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit interactionInfo;
+        if (Physics.Raycast(interactionRay, out interactionInfo, Mathf.Infinity))
+        {
+            GameObject interactedObject = interactionInfo.collider.gameObject;
+            if (interactedObject.tag == "Interactable Object")
+            {
+                Debug.Log("Interactable interacted");
+            }
+            else
+            {
+                playerAgent.destination = interactionInfo.point;
+            }
+        }
+    }
+
 }
